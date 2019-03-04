6e71d6ae193f3fe9d8dc11db034f6d3cb4a64c40
Faycal Hammoudi <fay68390@gmail.com>
2019-03-04 09:24:08 +0100

    inclure script et style

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
         <footer>
             
         </footer>
+        <script src="script.js"></script>
     </body>
-</html>
+</html> 
--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>HSS</title>
+    <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
