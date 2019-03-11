785ed0540b77d8c4a03200a5b636155e25c91e90
Faycal Hammoudi <fay68390@gmail.com>
2019-03-11 10:30:19 +0100

    newsletter

--- footer.php
+++ footer.php
@@ -1,14 +1,8 @@
             <footer>
-<<<<<<< HEAD
-                <div class="container row">
-                   <img class="img_footer" src="http://<?php echo $_SERVER['SERVER_NAME'] ?>/hss/logo.png" alt="Logo HSS">
-
-=======
                 <div class="container footer_inner">
                    <img class="img_footer" src="logo.png" alt="Logo HSS">
                    <h4>Newsletter</h4>
-                    <input class="input_footer" type="email" placeholder="exemple@contact.com"></input>
->>>>>>> 2d3930d6de8aa3e8171b7e0a95eb8bd87e86eedd
+                    <input class="input_footer" type="email" placeholder="exemple@contact.com">
                 </div>
             </footer>
         </div> <!-- WRAPPER -->
