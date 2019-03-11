e2235a40526d96df6ef011d6e62a6c994dd20767
Vanessa <vanessadettwiller@gmail.com>
2019-03-11 13:46:54 +0100

    Correction faute

--- contact/index.php
+++ contact/index.php
@@ -21,12 +21,12 @@
     
 
     <div class=" contactPage2">
-           <p class="contactTxt">Vous pouvez également nous contacter par courier postal : </p><br>
+           <p class="contactTxt">Vous pouvez également nous contacter par courrier postal : </p><br>
            <p class="contactTxt" style="color:black;">Groupe HSS</p>
            <p class="contactTxt" style="color:black;">3 rue de la gare</p>
            <p class="contactTxt" style="color:black;">68100 Mulhouse</p><br>
 
-           <p class="contactTxt">Ou par courier électronnique : </p><br>
+           <p class="contactTxt">Ou par courrier électronnique : </p><br>
            <p class="contactTxt" style="color:black;">contact@hss.com</p><br>
 
            <p class="contactTxt">Si vous souhaitez en savoir plsu sur le groupe, vous pouvez télécharger notre dossier de presse sur la page documents de presse</p>
