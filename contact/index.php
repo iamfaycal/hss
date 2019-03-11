35f33a2abaa37c312eff64af67ce3e71b217b697
Vanessa <vanessadettwiller@gmail.com>
2019-03-11 13:43:29 +0100

    Page de contact

--- contact/index.php
+++ contact/index.php
@@ -1,5 +1,38 @@
 <?php include("../header.php") ?>
 <section class="main">
-        
+       <h2>Nous contacter</h2> 
+    <div class="container row " style= "margin-top: 35px;">
+        <div class="contactPage1">
+            <form action="" method="post">
+            <p>
+                <label for="name">Nom :</label><br>
+                <input type="text" id="name" name="user_name">
+            </p>
+            <p>
+                <label for="mail">E-mail :</label><br>
+                <input type="email" id="mail" name="user_mail">
+            </p>
+            <p>
+                <label for="msg">Message :</label><br>
+                <textarea id="msg" name="user_message"></textarea>
+            </p>
+        </form>
+        </div>
+    
+
+    <div class=" contactPage2">
+           <p class="contactTxt">Vous pouvez également nous contacter par courier postal : </p><br>
+           <p class="contactTxt" style="color:black;">Groupe HSS</p>
+           <p class="contactTxt" style="color:black;">3 rue de la gare</p>
+           <p class="contactTxt" style="color:black;">68100 Mulhouse</p><br>
+
+           <p class="contactTxt">Ou par courier électronnique : </p><br>
+           <p class="contactTxt" style="color:black;">contact@hss.com</p><br>
+
+           <p class="contactTxt">Si vous souhaitez en savoir plsu sur le groupe, vous pouvez télécharger notre dossier de presse sur la page documents de presse</p>
+
+    </div>
+
+    </div>
 </section>
 <?php include("../footer.php"); ?>
