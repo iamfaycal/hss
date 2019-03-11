fa43ce2a8dc9ac56351707a599457c5423602e31
Faycal Hammoudi <fay68390@gmail.com>
2019-03-11 12:48:18 +0100

    page accueil

--- index.php
+++ index.php
@@ -1,6 +1,48 @@
 <?php include("home-header.php") ?>
-<section class="main">
-    
+<section class="main" style="text-align: center;">
+    <div class="container">
+        <h1>Harmonised Soul System</h1>
+        <h2>Qu'est-ce que c'est ?</h2>
+        <p>
+        Harmonised Soul System (HSS), est un groupe mulhousien formé en janvier 2018. Il est composé de trois jeunes d’une vingtaine d’année. HHS est une troupe originale et innovante dans son genre. Elle a la capacité de combiner deux styles artistiques différents, le breakdance et le théâtre en une représentation ce qui donne un caractère unique au spectacle.
+        </p>
+        <a href="/hss/a-propos" class="button">En savoir plus</a>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <h2>Nos prochains spéctacles</h2>
+        <div class="event">
+            <div class="event-poster">
+                <img src="http://<?php echo $_SERVER['SERVER_NAME'] ?>/hss/affiche.jpg" alt="affiche-nous">
+            </div>
+            <div class="event-content">
+                <h3 class="title">NOUS - 2019</h3>
+                <p>“Nous” est un spectacle combinant du breakdance et du théâtre racontant en mouvement la rencontre des membres de la troupe. Pendant 1h, à travers  ces deux styles artistiques, ils vont aborder des thématiques différentes.
+                Avec une authenticité rare, ils parlerons des défis qu’ils ont vécus et ceux qui les attendent.
+                Leur technique plonge les spectateurs dans un univers surprenant et pleins d’émotions, ils veulent permettent aux personnes de non pas seulement regarder mais de vivre leur histoire en même temps qu’eux.
+                </p>
+                <ul>
+                    <li>
+                        <h3>MULHOUSE - Scène de rue</h3>
+                        <p>19 juillet 2019 à 20h15</p>
+                    </li>
+                    <li>
+                        <h3>STRASBOURG - Fête de la bière</h3>
+                        <p>23 juillet 2019 à 20h30</p>
+                    </li>
+                    <li>
+                        <h3>NANCY - Festival Art’n’dance</h3>
+                        <p>03 août 2019 à 19h30</p>
+                    </li>
+                    <li>
+                        <h3>PARIS - Spectacle de rue</h3>
+                        <p>15 août 2019 à 20h00</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </section>
 <?php include("footer.php"); ?>
     
