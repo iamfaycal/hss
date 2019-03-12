955100f80f883b55c50d9f22284471004c621f46
Vanessa <vanessadettwiller@gmail.com>
2019-03-12 13:25:12 +0100

    Mentions légales

--- mentions-legales/index.php
+++ mentions-legales/index.php
@@ -1,5 +1,43 @@
 <?php include("../header.php") ?>
 <section class="main">
-    
+    <div class="container row" style="justify-content: space-between;">
+        <div class="col1">
+            <h3>Editeur du Site </h3>
+            <p class="pmention"><b>Responsable éditorial :</b> Ben Mariot</p>
+            <p class="pmention"><b>Adresse :</b> 3 rue de la Gare, 68100 Mulhouse</p>
+            <p class="pmention"><b>Téléphone :</b> 0633200080</p>
+            <p class="pmention"> <b>Email :</b> contact@hss.fr</p>
+            <p class="pmention"><b>Site Web :</b> www.harmonised-soul-system.fr</p>
+        </div>
+
+    <div class="col2">
+        <h3>Hébergement </h3>
+        <p class="pmention"><b>Hébergeur :</b> Agence 507</p>
+        <p class="pmention"><b>Adresse :</b> 9 Rue Louis Pasteur, 68100 Mulhouse</p>
+        <p class="pmention"><b>Téléphone :</b> 0602356993</p>
+        <p class="pmention"> <b>Email :</b> contact@cinqcentsept.fr</p>
+        <p class="pmention"><b>Site Web :</b> www.agence-507.fr</p>
+    </div>
+
+    <div class="col3">
+        <h3>Développement </h3>
+        <p class="pmention"> Agence 507</p>
+        <p class="pmention"><b>Adresse :</b> 9 Rue Louis Pasteur, 68100 Mulhouse</p>
+        <p class="pmention"><b>Téléphone :</b> 0602356993</p>
+        <p class="pmention"> <b>Email :</b> contact@cinqcentsept.fr</p>
+        <p class="pmention"><b>Site Web :</b> www.agence-507.fr</p>
+    </div>
+    </div>
+
+    <div class="container">
+        <div class="condition">
+            <h3>Accès au site internet</h3>
+            <p class="pmention">Le site internet HSS est accessible à l’adresse URL www.harmonised-soul-system.fr, 24h/24, 7 jours/7, sauf cas de force majeure, pannes informatiques, opérations de maintenance, de mise à jour ou de problèmes liés aux réseaux de télécommunication.
+L’accès, les informations et les services proposés sur le site internet HSS sont gratuits.</p>
+
+<h3>Propriété intellectuelle</h3>
+            <p class="pmention">Les pages internet, le texte, les photographies et les vidéos présentées sur le site www.harmonised-soul-system.fr partagés sont, sauf mention contraire, des créations originales du Groupe HSS.</p>
+        </div>
+    </div>
 </section>
 <?php include("../footer.php") ?>
