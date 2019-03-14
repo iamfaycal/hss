f95eefed6356eb9ed8b507d57474b550645575e4
Vanessa <vanessadettwiller@gmail.com>
2019-03-14 13:05:12 +0100

    Qui somme nous

--- qui-sommes-nous/index.php
+++ qui-sommes-nous/index.php
@@ -4,36 +4,21 @@
 
             <div class="container row" style="margin-bottom: 135px;">
             <div class="colG">
+            <h3 class="qsn">Ben vu par Noémie</h3>
                 <img src="Ben.jpg" class="img_round">
+                <p class="pmention">“Ben est un vrai curieux avec une imagination débordante. Il est toujours de bonne humeur et il adore sortir des sentiers battues. Mendy et moi on découvre énormément de choses auxquelles on ne se seraient jamais intéressé et ça nous aide dans nos processus créatifs.”
+                </p>
             </div>
-            <div class="colD">
-                    <h3 class="qsn">Ben vu par Noémie</h3>
-                    <p>“Ben est un vrai curieux avec une imagination débordante. Il est toujours de bonne humeur et il adore sortir des sentiers battues. Mendy et moi on découvre énormément de choses auxquelles on ne se seraient jamais intéressé et ça nous aide dans nos processus créatifs.”
-                    </p>
-            </div>
-
-            </div>
-
-            <div class="container row" style="margin-bottom: 135px;">
-            <div class="colG" >
-            <h3 class="qsn">Noémie vu par Mendy</h3>
-                    <p>“Noémie ou l’effrontée ! Elle n’hésite pas à imposer ses idées mais elle sait garder l’esprit de notre groupe. Elle apporte beaucoup de dynamisme et elle est très déterminée. Elle réussit à nous communiquer son énergie et elle a toujours les mots pour nous pousser plus loin !”
-                    </p>
-                
-            </div>
-            <div class="colD">
-            <img src="noemie.jpg" class="img_round">
-            </div>
-
-            </div>
-
-             <div class="container row" style="margin-bottom: 135px;">
             <div class="colG">
-                <img src="mendy.jpg" class="img_round">
+            <h3 class="qsn">Noémie vu par Mendy</h3>
+            <img src="noemie.jpg" class="img_round">
+                <p class="pmention">“Noémie ou l’effrontée ! Elle n’hésite pas à imposer ses idées mais elle sait garder l’esprit de notre groupe. Elle apporte beaucoup de dynamisme et elle est très déterminée. Elle réussit à nous communiquer son énergie et elle a toujours les mots pour nous pousser plus loin !”
+                    </p>
             </div>
-            <div class="colD">
-                    <h3 class="qsn">Mendy vu par Ben</h3>
-                    <p>“Mendy c’est le plus sage de nous trois ... et heureusement ! Il est là pour nous rattraper quand Noémie et moi on part trop loin. Il est très terre à terre mais ça ne l’empêche pas d’avoir une vision des choses très avant-gardiste aussi. On adore quand il écrit des textes pour nos spectacles, on est un peu ses premiers fans même si on fait partie du même groupe !”
+            <div class="colG">
+            <h3 class="qsn">Mendy vu par Ben</h3>
+                <img src="mendy.jpg" class="img_round">
+                <p class="pmention">“Mendy c’est le plus sage de nous trois ... et heureusement ! Il est là pour nous rattraper quand Noémie et moi on part trop loin. Il est très terre à terre mais ça ne l’empêche pas d’avoir une vision des choses très avant-gardiste aussi. On adore quand il écrit des textes pour nos spectacles, on est un peu ses premiers fans même si on fait partie du même groupe !”
                     </p>
             </div>
 
